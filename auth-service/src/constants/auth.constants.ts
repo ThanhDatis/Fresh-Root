11ba22677cd4c12971da0d3f41aafdb58252ea42
thanhdat1112004@gmail.com <2251120340@ut.edu.vn>
2026-07-30 00:14:11 +0700

    feat(auth): add authentication constants

---
+++ auth-service/src/constants/auth.constants.ts
@@ -0,0 +1,7 @@
+export const REFRESH_TOKEN_COOKIE_NAME = 'refreshToken';
+export const REFRESH_TOKEN_COOKIE_PATH = '/auth/refresh-token';
+export const REFRESH_TOKEN_COOKIE_MAX_AGE_MS = 7 * 24 * 60 * 60 * 1000; // 7 ngày
+
+export const RESET_TOKEN_BYTE_LENGTH = 32;
+
+export const BCRYPT_SALT_ROUNDS = 10;
