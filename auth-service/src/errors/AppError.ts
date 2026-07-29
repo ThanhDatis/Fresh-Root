3b2f7de75f44ebba800ed549d50e86d6e32fa15e
thanhdat1112004@gmail.com <2251120340@ut.edu.vn>
2026-07-30 00:19:41 +0700

    feat(auth): add authentication errors

---
+++ auth-service/src/errors/AppError.ts
@@ -0,0 +1,13 @@
+import type { ApiError } from '../types/apiResponse.types';
+
+export class AppError extends Error {
+  constructor(
+    public statusCode: number,
+    message: string,
+    public code: string,
+    public errors?: ApiError[],
+  ) {
+    super(message);
+    this.name = 'AppError';
+  }
+}
